68150759ae4e74986386906a46fa7c32dc14b955
Hathos_Vanox <49489547+AidanMahoney73@users.noreply.github.com>
2020-09-27 23:53:53 -0400

    turn in

--- adv.py
+++ adv.py
@@ -25,12 +25,60 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
+
+
+
+
+
+
+
+
+
+def get_travel_order():
+    #* init travel log and memory
+    memory = dict()
+    travel_log = []
+    start_room = player.current_room.id
+        #? Room Stutus Codes (White = Unvisited), (Grey = Visted but Uncompleted), (Black = Completely Vistited)
+    memory[start_room] = ["Start", "Grey"] # [Descovered_by, Room_Status]
+
+    while_loop_fail_safe = 0
+    while memory[start_room][1] != "Black":
+        while_loop_fail_safe += 1
+        if (while_loop_fail_safe % 2000) == 0:
+            memory[start_room][1] = "Black"
+            print("You Failed")
+            
+    return travel_log
+
+traversal_path = get_travel_order()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
-traversal_path = []
+# traversal_path = []
+#'e', 'n', 'e', 'n', 'e', 'n', 'e', 'n', 'e', 'n', 'n', 'e', 'n', 'n', 'e', 'e', 'e', 'e'
 
 
-
+#! ==================== DO NOT MODIFY ====================
 # TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
 player.current_room = world.starting_room
@@ -45,18 +93,19 @@ if len(visited_rooms) == len(room_graph):
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
+#! ==================== DO NOT MODIFY ====================
 
 
 
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
